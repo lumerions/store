f61568e920d2580c128f160c63c74f387062b043
lumerions <asdfghjklzxcvbnm20231@gmail.com>
2026-03-25 20:58:49 +0000

    de

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI,Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(
     title="Vercel + FastAPI",
