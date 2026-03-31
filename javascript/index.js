9ceac444ab5eef2942f973a8aa74b6a7b6a9e0c4
unknown <asdfghjklzxcvbnm20231@gmail.com>
2026-03-31 18:33:30 -0400

    ee

--- javascript/index.js
+++ javascript/index.js
@@ -71,7 +71,7 @@ function renderCart() {
                         </div>
                     </div>
                     
-                    <button class="cart-item-remove" onclick="removeItem(${index})" title="Remove Item">
+                    <button class="cart-item-remove" onclick="removeItem(${item.id})" title="Remove Item">
                         <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <polyline points="3 6 5 6 21 6"></polyline>
                             <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
@@ -112,7 +112,7 @@ function processPayment(method) {
     showNotification("Success! Your order has been placed.", "success");
 }
 
-updateCountBadge();
+updateCountBadge()
 
 async function CheckIfUserLoggedIn() {
     try {
