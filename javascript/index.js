dc8c0248f5eb84c68178e15896b146b53729345c
unknown <asdfghjklzxcvbnm20231@gmail.com>
2026-03-31 18:28:20 -0400

    ee

--- javascript/index.js
+++ javascript/index.js
@@ -66,15 +66,11 @@ function renderCart() {
                             <h4 class="cart-item-name">${item.name}</h4>
                             <p class="cart-item-price">${item.price}</p>
                         </div>
-                        
                         <div class="cart-item-img-container">
                             <img src="${item.image}" alt="${item.name}" class="cart-item-thumb">
                         </div>
                     </div>
-
-                    <button class="cart-item-remove" onclick="removeItem(${index})">
-                        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
-                    </button>
+                    <button class="cart-item-remove" onclick="removeItem(${index})">&times;</button>
                 </div>
             `;
         });
