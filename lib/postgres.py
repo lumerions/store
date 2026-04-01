d4aca5df88bf3ea07592248633af269eb5bf43e3
lumerions <asdfghjklzxcvbnm20231@gmail.com>
2026-04-01 22:03:24 +0000

    ee

--- lib/postgres.py
+++ lib/postgres.py
@@ -2,7 +2,15 @@ import psycopg
 from psycopg_pool import ConnectionPool
 from config import Config
 cfg = Config()
-pool = ConnectionPool(cfg.PostgresConnection)
+pool = ConnectionPool(
+    cfg.PostgresConnection,
+    min_size=1,
+    max_size=10,
+    check=ConnectionPool.check_connection, 
+    max_idle=30.0,                         
+    max_lifetime=300.0,                    
+    reconnect_timeout=10.0
+)
 
 def getPostgresConnection():
     return pool.connection()
