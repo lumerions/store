dd8e58c637c4cd019bef35fd99f4e1b652fec5b8
lumerions <asdfghjklzxcvbnm20231@gmail.com>
2026-04-01 21:56:25 +0000

    ee

--- lib/config.py
+++ lib/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Config(BaseSettings):
     RedisConnectionToken: str = "AfvMAAIncDFlYzAyODM4OGM0Nzg0ODBlODdkYzEyZTY3ZjExOWU3MHAxNjQ0NjA"
     RedisConnectionURL: str = "https://steady-heron-64460.upstash.io"
-    PostgresConnection: str = "postgresql://neondb_owner:npg_nuc5NPZl0DJA@ep-tiny-waterfall-adb4f3n3-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+    PostgresConnection: str = "postgresql://neondb_owner:npg_nuc5NPZl0DJA@ep-tiny-waterfall-adb4f3n3-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require&connect_timeout=10&keepalives=1&keepalives_idle=30"
     StoreName: str = "MODERN"
     AdminUsername: str = "Admin"
     ResendAPIKey: str = "re_Pwq37GMA_BbyuATLy4p5eQhUWE9FyRwjy"
